4d1a2748b14cdf4d0179cc51065275f820aa65ba
RhineLT <69440427+RhineLT@users.noreply.github.com>
2024-09-24 11:22:07 +0800

    增加媒体支持

--- ffmpeg_batch.sh
+++ ffmpeg_batch.sh
@@ -7,10 +7,13 @@ OUTPUT_DIR="${PWD}/output"
 mkdir -p "${OUTPUT_DIR}"
 
 # Loop through all video files in the current directory
-for file in *.mp4 *.mkv *.avi *.mov *.wmv; do
+for file in *.mp4 *.mkv *.avi *.mov *.wmv *.MP4 *.MOV *.m4v *.mpg; do
   # Get the file name without extension
   filename="${file%.*}"
 
-  # Compress the video file using ffmpeg
-  ffmpeg -i "${file}" -c:v libx264 -crf 26 "${OUTPUT_DIR}/${filename}.mp4"
+  # Set the CRF value
+  CRF_VALUE=26
+  
+  ffmpeg -i "${file}" -c:v libx264 -crf ${CRF_VALUE} -r 30 "${OUTPUT_DIR}/${filename}_batch_${CRF_VALUE}.mp4"
+
 done
